8911b4102c6b325a027d373393ffc6b5474a68a3
b195fea <b195fea@gmail.com>
2022-10-28 18:05:01 +0800

    增加 java11 Nest-Based Access Control 說明

---
+++ java11_demo/src/idv/zjh/java11/NestAccessExample.java
@@ -0,0 +1,33 @@
+package idv.zjh.java11;
+
+import java.lang.reflect.Field;
+
+
+/**
+ * <h1>Nest-Based Access Control</h1>
+ * <h2>基於嵌套類（內部類）的私有屬性訪問控制</h2>
+ * 網址訪問：https://openjdk.org/projects/jdk/11/
+ * 
+ * 內部類可以互相訪問私有屬性
+ * @author zjh
+ *
+ */
+public class NestAccessExample {
+	public static class X {
+		void test() throws Exception {
+			Y y = new Y();
+			y.y = 1;
+			Field field = Y.class.getDeclaredField("y");
+			field.setAccessible(true);//java 11 之前
+			field.setInt(y, 1);
+		}
+	}
+
+	private static class Y {
+		private int y;
+	}
+
+	public static void main(String[] args) throws Exception {
+		new X().test();
+	}
+}
