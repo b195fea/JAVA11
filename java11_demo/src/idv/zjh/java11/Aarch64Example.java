40faa18f9eb25e4740e9bc56270e5f587d58bd83
b195fea <b195fea@gmail.com>
2022-11-03 18:16:23 +0800

    JPE315 改進 aarch64 函數<

---
+++ java11_demo/src/idv/zjh/java11/Aarch64Example.java
@@ -0,0 +1,30 @@
+package idv.zjh.java11;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * <h1>JPE315 改進 aarch64 函數</h1>
+ * 改進在64位元上sin、cos、log的計算效率
+ *
+ */
+public class Aarch64Example {
+	
+	public static void mathOnJdk11() {
+		long startTime = System.nanoTime();
+		for (int i = 0;i<10000000;++i) {
+			Math.sin(i);
+			Math.cos(i);
+			Math.log(i);
+		}
+		long endTime = System.nanoTime();
+		System.out.println(TimeUnit.MILLISECONDS.toMillis(endTime-startTime)+"");
+		//時間顯示
+		//java8 	991184100	
+		//java11	889397900
+	}
+
+	public static void main(String[] args) {
+		mathOnJdk11();
+	}
+
+}
